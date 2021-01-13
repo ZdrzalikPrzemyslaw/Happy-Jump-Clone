c74ead5640d8ea6f41c73e639ed8b1ef469d01f6
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2021-01-13 19:35:47 +0100

    init present class

---
+++ app/src/main/java/tech/szymanskazdrzalik/weather_game/game/entities/PresentEntity.java
@@ -0,0 +1,13 @@
+package tech.szymanskazdrzalik.weather_game.game.entities;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+
+public class PresentEntity extends FriendlyEntity {
+
+    public PresentEntity(int xPos, int yPos, int textureWidth, int textureHeight, Bitmap background) {
+        super(xPos, yPos, textureWidth, textureHeight, background);
+    }
+
+}
